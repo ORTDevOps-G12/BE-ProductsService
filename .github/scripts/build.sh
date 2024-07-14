394cfdc2e5e3c34e18158e00e41ddcea037d5038
nconde-ort <nc241899@fi365.ort.edu.uy>
2024-07-14 18:21:18 -0300

    Correccion al verificar existencia de jar 3

--- .github/scripts/build.sh
+++ .github/scripts/build.sh
@@ -9,19 +9,22 @@ export COMMIT_DATE="$(git log --date=format:'%Y-%m-%d %H:%M:%S' --pretty="%cd" -
 printenv
 
 # Remove any existing build directory
-rm -rf ./out
-
-# Change directory
-cd codeSource
+rm -rf ./codeSource/out
 
 # Grant execute permission for Maven Wrapper
-chmod +x ./mvnw
+chmod +x ./codeSource/mvnw
 
 # Compile the project using Maven
-./mvnw clean package
+./codeSource/mvnw clean package
 
 # Create an output directory
-mkdir -p out
+mkdir -p ./codeSource/out
+
+# List contents of target directory for debugging
+ls -la ./codeSource/target
 
 # Copy the JAR file to the output directory
-cp target/*.jar out/
+cp ./codeSource/target/*.jar ./codeSource/out/
+
+# List contents of output directory for debugging
+ls -la ./codeSource/out
